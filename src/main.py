21099a55def7ddd2b1de1b3afc1a320162c390d5
104000045 <jacky860226@yahoo.com.tw>
2020-09-22 10:13:53 +0800

    2 method

--- src/main.py
+++ src/main.py
@@ -16,14 +16,7 @@ def process_command():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    arg = process_command()
-    parser = Parser.Parser()
-    if arg.input is not None:
-        inputStream = open(arg.input)
-    else:
-        inputStream = sys.stdin
-    rawModule = parser.parse(inputStream)
+def UpUpSolver(rawModule):
     module = Basic.Module(rawModule)
 
     delay_initialer = Up_delay_initialer(module)
@@ -41,15 +34,65 @@ if __name__ == '__main__':
         for delay in wire.output_delay.values():
             assert(delay == 0)
 
-    if arg.input is not None:
-        with open(arg.output, 'w') as outputStream:
-            module.verilog_output(outputStream)
-    else:
-        module.verilog_output(sys.stdout)
-    print(arg.input)
-    print("JJ level =", delay_initialer.max_delay() - 2)
-    print("JJ count =", module.get_JJ_count())
+    JJ_level = delay_initialer.max_delay() - 2
+    JJ_count = module.get_JJ_count()
+    print("JJ level =", JJ_level)
+    print("JJ count =", JJ_count)
     print("bufferNum =", inserter.buffer_cnt,
           ", splitterNum =", inserter.splitter_cnt)
     print("buffer decrease by decreaser:", dec_buffer)
+
+    return module, JJ_level, JJ_count, inserter.buffer_cnt, inserter.splitter_cnt
+
+
+def DownDownSolver(rawModule):
+    module = Basic.Module(rawModule)
+
+    delay_initialer = Down_delay_initialer(module)
+    leveler = Leveler(module, delay_initialer, 'D')
+    leveler.process()
+
+    inserter = Inserter.Buffer_splitter_inserter(module)
+    inserter.insert_splitter()
+    dec_buffer = inserter.buffer_decrease()
+    inserter.insert_buffer_and_splitter()
+
+    leveler.reset_unprocess_wire_delay()
+    leveler.cal_wire_delay()
+    for wire in module.wires:
+        for delay in wire.output_delay.values():
+            assert(delay == 0)
+
+    JJ_level = delay_initialer.max_delay() - 2
+    JJ_count = module.get_JJ_count()
+    print("JJ level =", JJ_level)
+    print("JJ count =", JJ_count)
+    print("bufferNum =", inserter.buffer_cnt,
+          ", splitterNum =", inserter.splitter_cnt)
+    print("buffer decrease by decreaser:", dec_buffer)
+
+    return module, JJ_level, JJ_count, inserter.buffer_cnt, inserter.splitter_cnt
+
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(1000000)
+    arg = process_command()
+    parser = Parser.Parser()
+    if arg.input is not None:
+        inputStream = open(arg.input)
+    else:
+        inputStream = sys.stdin
+    rawModule = parser.parse(inputStream)
+
+    print(arg.input)
+
+    UpUp_module = UpUpSolver(rawModule)[0]
+    DownDown_module = DownDownSolver(rawModule)[0]
+
     print("")
+
+    if arg.input is not None:
+        with open(arg.output, 'w') as outputStream:
+            UpUp_module.verilog_output(outputStream)
+    else:
+        UpUp_module.verilog_output(sys.stdout)
